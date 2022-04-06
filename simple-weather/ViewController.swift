735f8b27e3232954bb8608c0e1e29da0e486e789
yc3yu <cindy.yu@thescore.com>
2022-04-06 12:11:59 -0400

    Initial commit

---
+++ simple-weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simple-weather
+//
+//  Created by Cindy Yu on 2022-04-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
